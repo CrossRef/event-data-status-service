0369c7b610ebf6cea32efcfdaa4185cdb2e9d5cf
Joe Wass <joe@afandian.com>
2016-09-13 16:56:17 +0100

    License.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject event-data-status-service "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Status monitoring and reporting for Crossref Event Data"
+  :url "http://eventdata.crossref.org"
+  :license {:name "The MIT License (MIT)"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.12.0"]
                  [compojure "1.5.1"]
