9a70a2effca471c92ef97d32368121bad7197fe2
Joe Wass <joe@afandian.com>
2016-09-15 16:40:25 +0100

    Show counts. Tidyup.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-status-service "0.1.6"
+(defproject event-data-status-service "0.1.7"
   :description "Status monitoring and reporting for Crossref Event Data"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
