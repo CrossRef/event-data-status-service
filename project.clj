fdf32740995a268d6f3baea0ab88b51015365ab7
Joe Wass <joe@afandian.com>
2016-09-16 11:19:37 +0100

    Display in grid.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-status-service "0.1.8"
+(defproject event-data-status-service "0.1.9"
   :description "Status monitoring and reporting for Crossref Event Data"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
